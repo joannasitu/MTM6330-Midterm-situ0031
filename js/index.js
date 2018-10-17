e280a3afed2ed63e9a61fe7c6a9925929c4f1bb7
Joanna <situ0031@algonquinlive.com>
2018-10-17 15:27:21 -0400

    Midterm Final Touches

--- js/index.js
+++
@@ -1,7 +0,0 @@
-const menu = document.getElementById('menu')
-const nav = document.getElementById('nav')
-
-menu.addEventListener('click', function () {
-  menu.classList.toggle('close')
-  nav.classList.toggle('active')
-})
